1503d4328b20abead729dc9ee2f1cee3818d1229
hasbiyallah01 <oyebohm@gmail.com>
2024-03-10 09:08:25 +0100

    new commit

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const clientData = {
     6: "Balogun Muhammed-Awwal",
     7: "Ibrahim Odusanya",
     8: "Abdullah Oladejo",
-    9: "Dhulfikkar Animashaun",
+    9: "Dhulfikhar Animashaun",
     10: "Qaanitah Oyekola",
     11: "Usman Afolayan",
     12: "Ahmadraza Danmole",
@@ -24,6 +24,7 @@ const clientData = {
     24: "Adekoya Yusuf",
 };
 
+
 const taskCountMap = {
     sweeping: 3,
     serving: 9,
@@ -31,14 +32,29 @@ const taskCountMap = {
     "washing-plate": 9,
 };
 
+
 document.addEventListener("DOMContentLoaded", function () {
-    autoAssignTasks();
+    // runDailyTasksGeneration();
     updateTaskList();
+    // loadAttendanceList(); // Load attendance list when the page is loaded
 });
 
 
+
+
+// function runDailyTasksGeneration() {
+//     const storedDate = localStorage.getItem("tasksDate");
+//     const currentDate = new Date().toDateString();
+
+//     if (storedDate !== currentDate) {
+//         autoAssignTasks();
+//         localStorage.setItem("tasksDate", currentDate);
+//     }
+// }
+
 function autoAssignTasks() {
     const tasks = [];
+    // const currentDate = new Date();
     const femaleIds = [23, 4, 10, 15];
     Object.entries(taskCountMap).forEach(([taskType, count]) => {
         const clientIds = Object.keys(clientData).filter(id => {
@@ -61,7 +77,6 @@ function autoAssignTasks() {
 
 
 
-
 function updateTaskList() {
     const taskTableBody = document.getElementById("task-list");
     taskTableBody.innerHTML = "";
@@ -70,10 +85,11 @@ function updateTaskList() {
     if (!tasks || tasks.length === 0) {
         const noRecordRow = taskTableBody.insertRow();
         const noRecordCell = noRecordRow.insertCell();
-        noRecordCell.colSpan = 2;
+        noRecordCell.colSpan = 3; 
         noRecordCell.textContent = "No records found.";
         return;
     }
+
     const tasksByType = {};
     tasks.forEach(task => {
         if (!tasksByType[task.taskType]) {
@@ -88,8 +104,14 @@ function updateTaskList() {
             const row = taskTableBody.insertRow();
             const nameCell = row.insertCell();
             const taskCell = row.insertCell();
+            const attendanceCell = row.insertCell(); 
             nameCell.textContent = task.clientName;
             taskCell.textContent = task.taskType;
+            const attendanceCheckbox = document.createElement('input');
+            attendanceCheckbox.type = 'checkbox';
+            attendanceCheckbox.dataset.clientId = task.clientId; 
+            attendanceCheckbox.addEventListener('change', updateAttendance);
+            attendanceCell.appendChild(attendanceCheckbox);
         });
     });
 }
@@ -112,6 +134,14 @@ function shuffleArray(array) {
 document.getElementById("name-search").addEventListener("input", function () {
     search();
 });
+
+const attendanceForm = document.getElementById("attendance-form");
+attendanceForm.addEventListener("submit", function (event) 
+{
+    event.preventDefault(); 
+    updateAttendance(event); 
+});
+
 
 function search() {
     const searchInput = document.getElementById("name-search").value.trim().toLowerCase();
@@ -150,26 +180,132 @@ function search() {
     });
 }
 
+
+
+// function runDailyTasksGeneration() 
+// {
+//     const storedDate = localStorage.getItem("tasksDate");
+//     const currentDate = new Date().toDateString();
+
+//     if (storedDate !== currentDate) 
+//     {
+//         autoAssignTasks();
+//         localStorage.setItem("tasksDate", currentDate);
+//     }
+// }
+
+
+
+// function loadAttendanceList() 
+// {
+//     const attendanceList = JSON.parse(localStorage.getItem("attendanceList")) || {};
+//     const taskTableBody = document.getElementById("task-list");
+//     Object.keys(attendanceList).forEach(clientId => 
+//     {
+//         const attendance = attendanceList[clientId];
+//         const checkbox = taskTableBody.querySelector(`input[data-client-id="${clientId}"]`);
+//         if (checkbox) 
+//         {
+//             checkbox.checked = attendance;
+//         }
+//     });
+// }
+
+
 const captureButton = document.getElementById("convert-button");
 captureButton.addEventListener("click", captureTaskList);
 
-function captureTaskList() {
-	const taskList = document.getElementById("task-table");
-	html2canvas(taskList).then((canvas) => {
-		const imgData = canvas.toDataURL("image/png");
-		const downloadLink = document.createElement("a");
-		downloadLink.href = imgData;
-		downloadLink.download = "task-table.png";
-		downloadLink.click();
-	});
-}
-
-function autenticateNormalUser(params) {
-	//TODO: Add this to authenticate normal user
-}
-function authenticateAdminUser(params) {
-	//TODO:Add this to Authenticate admin user
+function captureTaskList() 
+{
+    const taskList = document.getElementById("task-table");
+    html2canvas(taskList).then((canvas) => 
+    {
+        const imgData = canvas.toDataURL("image/png");
+        const downloadLink = document.createElement("a");
+        downloadLink.href = imgData;
+        downloadLink.download = "task-table.png";
+        downloadLink.click();
+    });
 }
 
 
+function updateAttendance(event) {
+    const clientId = event.target.dataset.clientId;
+    const attendance = event.target.checked;
+    const attendanceList = JSON.parse(localStorage.getItem("attendanceList")) || {};
+    attendanceList[clientId] = attendance;
+    localStorage.setItem("attendanceList", JSON.stringify(attendanceList));
 
+    if (!attendance) {
+        const tasks = retrieveTasks();
+        const updatedTasks = tasks.filter(task => task.clientId !== clientId);
+        localStorage.setItem("tempAbsentTasks", JSON.stringify(updatedTasks));
+    } else 
+    {
+        const tempAbsentTasks = JSON.parse(localStorage.getItem("tempAbsentTasks")) || [];
+        const updatedTempAbsentTasks = tempAbsentTasks.filter(task => task.clientId !== clientId);
+        localStorage.setItem("tempAbsentTasks", JSON.stringify(updatedTempAbsentTasks));
+    }
+
+    if (event.type === 'submit') {
+       
+        autoAssignTasks(); 
+        updateTaskList(); 
+    }
+}
+
+
+
+
+
+// function updateAttendance(event) 
+// {
+//     const clientId = event.target.dataset.clientId;
+//     const attendance = event.target.checked;
+//     const attendanceList = JSON.parse(localStorage.getItem("attendanceList")) || {};
+//     attendanceList[clientId] = attendance;
+//     localStorage.setItem("attendanceList", JSON.stringify(attendanceList));
+
+//     if (attendance) 
+//     {
+        
+//         const tempAbsentTasks = JSON.parse(localStorage.getItem("tempAbsentTasks")) || [];
+//         const updatedTempAbsentTasks = tempAbsentTasks.filter(task => task.clientId !== clientId);
+//         localStorage.setItem("tempAbsentTasks", JSON.stringify(updatedTempAbsentTasks));
+//     } else 
+//     {
+//         const tasks = retrieveTasks();
+//         const updatedTasks = tasks.filter(task => task.clientId !== clientId);
+//         localStorage.setItem("tempAbsentTasks", JSON.stringify(updatedTasks));
+//     }
+
+//     if (event.type === 'submit') {
+//         const tasks = retrieveTasks();
+//         const presentClientIds = Object.keys(attendanceList).filter(clientId => attendanceList[clientId]);
+//         const presentTasks = tasks.filter(task => presentClientIds.includes(task.clientId));
+//         localStorage.setItem("tasks", JSON.stringify(presentTasks));
+//         autoAssignTasks(); 
+//         updateTaskList(); 
+//     }
+// }
+
+
+
+// function retrieveTasks() 
+// {
+//     const tasks = JSON.parse(localStorage.getItem("tempAbsentTasks")) || [];
+//     return tasks;
+// }
+
+
+
+
+
+function autenticateNormalUser(params) 
+{
+    //TODO: Add this to authenticate normal user
+}
+function authenticateAdminUser(params) 
+{
+    //TODO:Add this to Authenticate admin user
+}
